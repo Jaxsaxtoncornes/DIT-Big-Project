f36a503308616d79b2e4bdd1082f0ef6a14c52ba
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2025-05-26 11:44:33 +1200

    Allows you to spend strength to buy upgrades which remove your gems, will add the feature soon where it actually gives a upgrade

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 let strength = document.querySelector('.strength-cost')
+let parsedstrength = parseFloat(strength.innerHTML)
+
+let ClickerCost = document.querySelector('.drink-cost')
+let parsedClickerCost = parseFloat(ClickerCost.innerHTML)
 
 function incrementStrength() {
-strength.innerHTML = parseFloat(strength.innerHTML) + 1
+    parsedstrength += 1
+    strength.innerHTML = parsedstrength
+}
+
+function buyDrink() {
+    if (parsedstrength >= parsedClickerCost) {
+    parsedstrength -= parsedClickerCost
+    strength.innerHTML = parsedstrength
+    }
 }
