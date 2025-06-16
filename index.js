3f96b057c4c3a082badf2ad4ef07807d1f8c53b1
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2025-06-17 09:57:55 +1200

    fixed the issue, was a wrong variable name used in a certain place

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 let strength = document.querySelector('.strength-cost')
-let parsedstrength = parseFloat(strength.innerHTML)
+let parsedStrength = parseFloat(strength.innerHTML)
 
 let ClickerCost = document.querySelector('.drink-cost')
 let parsedClickerCost = parseFloat(ClickerCost.innerHTML)
 let drinklevel = document.querySelector('.drink-level')
 let drinkIncrease = document.querySelector('.drink-increase')
-let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
+let parsedClickerIncrease = parseFloat(drinkIncrease.innerHTML)
 
 let spc = 1;
 
 function incrementStrength() {
-    parsedstrength += spc;
-    strength.innerHTML = parsedstrength;
+    parsedStrength += spc;
+    strength.innerHTML = parsedStrength;
 }
 
 function buyDrink() {
-    if (parsedstrength >= parsedClickerCost) {
-    parsedstrength -= parsedClickerCost
-    strength.innerHTML = parsedstrength;
+if (parsedStrength >= parsedClickerCost) {
+    parsedStrength -= parsedClickerCost
+    strength.innerHTML = parsedStrength;
 
     drinklevel.innerHTML ++
 
-    parsedClickerIncrease = parsedClickerIncrease * 1.03
-    clickerIncrease.innerHTML = parsedClickerIncrease
-    spc += parsedClickerIncrease
+    parsedClickerIncrease = parsedClickerIncrease * 1.03;
+    drinkIncrease.innerHTML = parsedClickerIncrease;
+
+    spc += parsedClickerIncrease;
     }
 }
