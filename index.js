8550385c72ab612886cfc3b2e4c4689c9c476b09
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2025-06-17 09:50:37 +1200

    Issue in the code where when I click the boxing bag I don't gain strength

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ let parsedstrength = parseFloat(strength.innerHTML)
 let ClickerCost = document.querySelector('.drink-cost')
 let parsedClickerCost = parseFloat(ClickerCost.innerHTML)
 let drinklevel = document.querySelector('.drink-level')
+let drinkIncrease = document.querySelector('.drink-increase')
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
 
+let spc = 1;
 
 function incrementStrength() {
-    parsedstrength += 1
-    strength.innerHTML = parsedstrength
+    parsedstrength += spc;
+    strength.innerHTML = parsedstrength;
 }
 
 function buyDrink() {
     if (parsedstrength >= parsedClickerCost) {
     parsedstrength -= parsedClickerCost
-    strength.innerHTML = parsedstrength
+    strength.innerHTML = parsedstrength;
+
+    drinklevel.innerHTML ++
+
+    parsedClickerIncrease = parsedClickerIncrease * 1.03
+    clickerIncrease.innerHTML = parsedClickerIncrease
+    spc += parsedClickerIncrease
     }
 }
