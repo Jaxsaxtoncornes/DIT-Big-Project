04f0218c41f5cb97fe69780f3f1fc32d146ee0a6
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2025-08-25 17:26:32 +1200

    started to create the y frame and buttons for the skills and arifacts ill be adding

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { powerUpIntervals, upgrades } from "./constants/upgrades.js";
 import { defaultSkillValues, defaultUpgradeValues } from "./constants/defaultValues.js";
 
-let strength = document.querySelector(".strength")
-let parsedStrength = parseFloat(strength.innerHTMl)
+let strength = document.querySelector(".strength");
+let parsedStrength = parseFloat(strength.innerHTMl);
+
+let spcText = document.getElementById("spc-text");
+let spsText = document.getElementById("sps-text");
+
+let strengthImgContainer = document.querySelector(".strength-img-container");
+
+let upgradesNavButton = document.getElementById("upgrade-nav-button");
+let skillsNavButton = document.getElementById("skills-nav-button");
+let artifactsNavButton = document.getElementById("artifacts-nav-button");
